168bf35c1c94f5063f5fad293620a90363921814
Alexander Ernst <alexander.ernst@student.ipso.ch>
2023-06-19 23:00:36 +0200

    Formatierung

--- AarauCoinMVC/Models/Coin.cs
+++ AarauCoinMVC/Models/Coin.cs
@@ -8,6 +8,7 @@ namespace AarauCoinMVC.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public User UserId { get; set; }
 
         public double Coins { get; set; }
--- AarauCoinMVC/Services/IDatabaseCon.cs
+++ AarauCoinMVC/Services/IDatabaseCon.cs
@@ -1,15 +1,15 @@
 ﻿using AarauCoinMVC.Models;
-using NuGet.Protocol.Plugins;
 
 namespace AarauCoinMVC.Services
 {
     public interface IDatabaseCon
     {
-        public UserLoginDTO? GetUser(string username);
+        UserLoginDTO? GetUser(string username);
 
-        public AccountViewModel? GetUserInformation(string username);
+        AccountViewModel? GetUserInformation(string username);
 
-        public List<LogViewModel> ReadLog(string date);
+        List<LogViewModel> ReadLog(string date);
+
         List<string> GetUserNames();
 
         void SendMoney(string sender, string receiver, double amount);
